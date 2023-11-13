687a5dfe8454a9156e8eb0309028a875aff91448
suhaib <suhaibvbm@gmail.com>
2023-11-13 20:28:16 +0530

    created dictionary csv to dict

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import pandas
 import random
 import smtplib
 
+#reading csv
+data = pandas.read_csv("birthdays.csv")
+now = datetime.datetime.now()
+#creating a tuple of today month and day
+today = (now.month,now.day)
+
+birthday_dict = {(data_row["month"],
+                  data_row["day"]): data_row for index, data_row in data.iterrows()}
