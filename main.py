c8cd551e7f3140005f6a978b26a8e32e911fd61b
suhaib <suhaibvbm@gmail.com>
2023-11-13 20:31:00 +0530

    checking to day == penson day and replace name to person name (txt)

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ today = (now.month,now.day)
 
 birthday_dict = {(data_row["month"],
                   data_row["day"]): data_row for index, data_row in data.iterrows()}
+if today in birthday_dict:      #it check key only in method
+    with open(f"./letter_templates/letter_{random.randint(1, 3)}.txt", "r") as letter:
+        changed = letter.read()
+    # Get the data_row for today's birthday
+    data_row = birthday_dict[today]
+
+    # Replace [NAME] with the actual name
+    modified_letter = changed.replace("[NAME]",data_row["name"])
